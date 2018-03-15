02bc0482e8d7a19cb7ca680b5e89c7c6ec80b623
Prem Randeria <pranderia@marinsoftware.com>
2018-03-15 15:40:22 -0700

    Fix cors

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var index = require('./routes/index');
 var users = require('./routes/users');
 var session = require('express-session')
