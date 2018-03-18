318a30b14fbd4b75bc3a173d7a6efd6e15cc7964
premranderia <prem1890@gmail.com>
2018-03-17 18:31:32 -0700

    Update app.js

--- app.js
+++ app.js
@@ -22,12 +22,11 @@ app.use(session({
 var whitelist = ['https://code-name-game.herokuapp.com', 'http://localhost:4200']
 var corsOptions = {
   origin: function (origin, callback) {
-    callback(null, true)
-    // if (whitelist.indexOf(origin) !== -1) {
-    //   callback(null, true)
-    // } else {
-    //   callback(new Error('Not allowed by CORS'))
-    // }
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
   }
 }
 
